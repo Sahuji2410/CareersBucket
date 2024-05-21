caa628d54bf4ca796c984e0d8c58a7bfd901d477
Sanskar Sahu <sahusanskar78@gmail.com>
2024-05-21 18:00:05 +0530

    updated views.py using APIview

--- CareersBucket/Apps/app/urls.py
+++ CareersBucket/Apps/app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from Apps.app.views import *
 
 urlpatterns = [
-    path('job-types/', JobTypeListCreateAPIView.as_view(), name='jobtype-list-create'),
-    path('job-types/<int:pk>/', JobTypeRetrieveUpdateDestroyAPIView.as_view(), name='jobtype-detail'),
-    path('job-descriptions/', JobDescriptionListCreateAPIView.as_view(), name='jobdescription-list-create'),
-    path('job-descriptions/<int:pk>/', JobDescriptionRetrieveUpdateDestroyAPIView.as_view(), name='jobdescription-detail'),
+    path('job-types/', JobTypeView.as_view(), name='jobtype-list-create'),
+    path('job-types/<int:pk>/', JobTypeDetailView.as_view(), name='jobtype-detail'),
+    path('job-descriptions/', JobDescriptionView.as_view(), name='jobdescription-list-create'),
+    path('job-descriptions/<int:pk>/', JobDescriptionDetailView.as_view(), name='jobdescription-detail'),
 ]
